25c0b8b63fbcff8c69a758692cf3f3142101241e
taskinnasif144 <taskinnasif144@gmail.com>
2025-08-22 05:17:16 +0600

    location model

---
+++ src/infrastructure/db/locationModel.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { DatabaseNames, LocationLevels } from "../../shared/utils/enums";
+
+export interface ILocation {
+  title: string;
+  level: LocationLevels;
+  parentLevel: mongoose.Schema.Types.ObjectId | null;
+}
+
+export interface ILocationDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ILocationModel extends mongoose.Model<ILocationDocument> {}
+
+const locationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    level: {
+      type: String,
+      enum: LocationLevels,
+      required: true,
+    },
+    parentLevel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: DatabaseNames.Location,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Location = mongoose.model<ILocationDocument, ILocationModel>(
+  DatabaseNames.Location,
+  locationSchema,
+  DatabaseNames.Location
+);
+
+export default Location;
